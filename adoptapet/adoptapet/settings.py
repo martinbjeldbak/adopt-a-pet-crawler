7e2915bfbd3444cd024a3c399f4266cd43fcca9b
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2019-08-24 14:39:17 +1000

    Add pipeline to insert items into mongodb

--- adoptapet/adoptapet/settings.py
+++ adoptapet/adoptapet/settings.py
@@ -64,9 +64,9 @@ COOKIES_ENABLED = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'adoptapet.pipelines.AdoptapetPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'adoptapet.pipelines.MongoPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
